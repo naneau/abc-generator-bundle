08922e20232f06fb3700ec21a396ff986192d075
Maurice Fonk <git@naneau.net>
2014-07-08 18:12:15 -0400

    initial commit

---
+++ src/AngryBytes/GeneratorBundle/Bundle.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Bundle.php
+ *
+ * @category        AngryBytes
+ * @package         GeneratorBundle
+ * @subpackage      Bundle
+ * @copyright       Copyright (c) 2014 Angry Bytes BV (http://www.angrybytes.com)
+ */
+
+namespace AngryBytes\GeneratorBundle;
+
+use Abc\AbcBundle\Bundle\Bundle as AbcBundle;
+
+/**
+ * Bundle
+ *
+ * GeneratorBundle's Bundle
+ *
+ * @category        AngryBytes
+ * @package         GeneratorBundle
+ * @subpackage      Bundle
+ */
+class Bundle extends AbcBundle {}
