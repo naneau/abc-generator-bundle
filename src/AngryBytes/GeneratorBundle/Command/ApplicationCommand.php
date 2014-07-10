34e3baf6609339d9d5af721b6e7e29777056c5ee
Maurice Fonk <git@naneau.net>
2014-07-10 15:40:17 -0400

    adding working application generation

--- src/AngryBytes/GeneratorBundle/Command/ApplicationCommand.php
+++ src/AngryBytes/GeneratorBundle/Command/ApplicationCommand.php
@@ -10,11 +10,20 @@
 
 namespace AngryBytes\GeneratorBundle\Command;
 
+use Abc\AbcBundle\Application\Collection as Applications;
+use Abc\AbcBundle\Project\Directories as DirectoryLayout;
+
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
 
-use Naneau\FileGen\Structure;
+use Symfony\Component\Console\Question\Question;
+
+use AngryBytes\GeneratorBundle\Structure\Application as Structure;
+
+use Naneau\FileGen\Console\Helper\ParameterHelper;
+use Naneau\FileGen\Generator;
 
 /**
  * ApplicationCommand
@@ -35,6 +44,20 @@ class ApplicationCommand extends Command
     private $structure;
 
     /**
+     * The directory layout
+     *
+     * @var DirectoryLayout
+     **/
+    private $directoryLayout;
+
+    /**
+     * Applications
+     *
+     * @var Applications
+     **/
+    private $applications;
+
+    /**
      * Configure the command
      *
      * @return void
@@ -43,18 +66,71 @@ class ApplicationCommand extends Command
     {
         $this
             ->setName('generator:application')
-            ->setDescription('Generate a new application');
+            ->setDescription('Generate a new application')
+            ->addOption(
+                'name',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'Name of the application (also used as the directory under which it is generated)'
+            );
     }
 
     /**
      * Execute the command
      *
+     * @param  InputInterface  $input
+     * @param  OutputInterface $output
      * @return void
      **/
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // Parameter helper
-        $helper = $this->getHelper('filegenParameters');
+        // Figure out name
+        $name = $input->getOption('name');
+        if (empty($name)) {
+            // Name question
+            $question = new Question('Please enter the location for the application: ');
+
+            // Ask for the name
+            $name = $this->getHelper('question')->ask($input, $output, $question);
+        }
+
+        // Create the structure
+        $structure = $this->getStructure()->createForName($name);
+
+        $parameters = array_merge(
+            array('name' => $name),
+            $this->getParameterHelper()->askParameters($structure, $input, $output)
+        );
+
+        $generator = new Generator(
+            $this->getDirectoryLayout()->getRoot() . '/applications',
+            $parameters
+        );
+
+        $generator->generate($structure);
+    }
+
+    /**
+     * Get the directory layout
+     *
+     * @return DirectoryLayout
+     */
+    public function getDirectoryLayout()
+    {
+        return $this->directoryLayout;
+    }
+
+    /**
+     * Set the directory layout
+     *
+     * @param  DirectoryLayout    $directoryLayout
+     * @return ApplicationCommand
+     */
+    public function setDirectoryLayout(DirectoryLayout $directoryLayout)
+    {
+        $this->directoryLayout = $directoryLayout;
+
+        return $this;
     }
 
     /**
@@ -70,7 +146,7 @@ class ApplicationCommand extends Command
     /**
      * Set the structure to generate
      *
-     * @param Structure $structure
+     * @param  Structure          $structure
      * @return ApplicationCommand
      */
     public function setStructure(Structure $structure)
@@ -78,6 +154,39 @@ class ApplicationCommand extends Command
         $this->structure = $structure;
 
         return $this;
+    }
+
+    /**
+     * Get the application set
+     *
+     * @return Applications
+     */
+    public function getApplications()
+    {
+        return $this->applications;
+    }
+
+    /**
+     * Set the application set
+     *
+     * @param  Applications       $applications
+     * @return ApplicationCommand
+     */
+    public function setApplications(Applications $applications)
+    {
+        $this->applications = $applications;
+
+        return $this;
+    }
+
+    /**
+     * Get the filegen parameter helper
+     *
+     * @return ParameterHelper
+     **/
+    private function getParameterHelper()
+    {
+        return $this->getHelper('filegenParameters');
     }
 
 }
--- src/AngryBytes/GeneratorBundle/Structure/Application.php
+++ src/AngryBytes/GeneratorBundle/Structure/Application.php
@@ -10,29 +10,60 @@
 namespace AngryBytes\GeneratorBundle\Structure;
 
 use Naneau\FileGen\Structure;
+use Naneau\FileGen\File\Contents\Twig as TwigContents;
+
+use \Twig_Environment as Twig;
 
 /**
  * Application
  *
- * Structure for an application
+ * Generates the structure for an application
  *
  * @category        AngryBytes
  * @package         GeneratorBundle
  * @subpackage      Structure
  */
-class Application extends Structure
+class Application
 {
     /**
-     * Constructor
+     * The twig template engine
      *
-     * @param string $root
-     * @return void
+     * @var Application
      **/
-    public function __construct($root)
-    {
-        parent::__construct($root);
+    private $twig;
 
-        $this
+    /**
+     * Get the twig template engine
+     *
+     * @return Twig
+     */
+    public function getTwig()
+    {
+        return $this->twig;
+    }
+
+    /**
+     * Set the twig template engine
+     *
+     * @param  Twig        $twig
+     * @return Application
+     */
+    public function setTwig(Twig $twig)
+    {
+        $this->twig = $twig;
+
+        return $this;
+    }
+
+    /**
+     * Create a structure for an application
+     *
+     * @return Structure
+     **/
+    public function createForName($name)
+    {
+        $structure = new Structure;
+        $structure
             // Root
             ->directory($name)
 
@@ -40,18 +71,48 @@ class Application extends Structure
             ->directory($name . '/application')
             ->directory($name . '/application/Resources')
             ->directory($name . '/application/Resources/config')
-            ->file($name . '/application/Resources/config/application.xml', new TwigContents(
-                __DIR__ . '/../Resources/templates/application/application.xml.twig'
-            ))
+            ->file(
+                $name . '/application/Resources/config/application.xml',
+                $this->createTwigContents('application.xml.twig')
+            )
+            ->file(
+                $name . '/application/Resources/config/menu.yml',
+                $this->createTwigContents('menu.yml.twig')
+            )
+            ->file(
+                $name . '/application/Resources/config/routing.yml',
+                $this->createTwigContents('routing.yml.twig')
+            )
 
             // Public webroot
             ->directory($name . '/public')
-            ->file($name . '/public/index.php', new TwigContents(
-                __DIR__ . '/../Resources/templates/application/front-controller.php.twig'
-            ));
+            ->file(
+                $name . '/public/index.php',
+                $this->createTwigContents('front-controller.php.twig')
+            );
 
         // Parameters
-        $this
-            ->param('name', 'Name of the application');
+        $structure
+            ->parameter('id', 'Application id');
+
+        return $structure;
     }
+
+    /**
+     * Create twig file contents
+     *
+     * @return TwigContents
+     **/
+    private function createTwigContents($name)
+    {
+        $fullPath = sprintf(
+            __DIR__ . '/../Resources/templates/%s',
+            $name
+        );
+
+        return new TwigContents(
+            $this->getTwig()->loadTemplate($fullPath)
+        );
+    }
+
 }
